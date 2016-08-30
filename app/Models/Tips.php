ff3dc9a4b4b2b512acd9789286040bf904c598a7
Henry Leon <Helg18@gmail.com>
2016-08-30 17:06:48 -0400

    Mostrar votaciones en Tips

--- app/Models/Tips.php
+++ app/Models/Tips.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Categorias;
+use App\Models\Tips_votaciones;
+use DB;
 
 class Tips extends Model
 {
@@ -22,7 +24,8 @@ class Tips extends Model
 			'comentario'   => $c->comentario,
 			'id_user'      => $c->id_user,
 			'categoria'    => $c->categoria($c->id_categoria),
-			'subcategoria' => $c->subcategoria($c->id_subcategoria)
+			'subcategoria' => $c->subcategoria($c->id_subcategoria),
+			'votos' => $c->votos($c->id)
 			];
 		}
 		return $data;
@@ -36,6 +39,13 @@ class Tips extends Model
 
 	public static function subcategoria($subcategoria){
 		return Subcategorias::find($subcategoria)->first(['subcategoria']);
+	}
+
+	public static function votos($tips_id){
+		return DB::table('tips_votaciones')
+                     ->select(DB::raw('TRUNCATE (avg(calificacion), 1) as calificacion, comentario'))
+                     ->where('id', $tips_id)
+                     ->get();
 	}
 
 
