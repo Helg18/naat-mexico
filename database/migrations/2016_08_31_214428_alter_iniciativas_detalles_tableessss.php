47614e00fa0a4ca774ba5a82ea99347c629eb0ad
Henry Leon <helg18@gmail.com>
2016-10-25 18:07:36 -0400

    Cambios de detalles

---
+++ database/migrations/2016_08_31_214428_alter_iniciativas_detalles_tableessss.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterIniciativasDetallesTableessss extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('iniciativas_detalles', function (Blueprint $table) {
+            $table->integer('pasosiniciativas_id')->unsigned();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('iniciativas_detalles', function (Blueprint $table) {
+            $table->dropColumn('pasosiniciativas_id');
+        });
+    }
+}
